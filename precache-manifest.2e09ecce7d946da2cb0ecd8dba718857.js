ac74d2f3a2ca31584da56d4946c01403cb3a5088
jalajcodes <Jalaj@mail.com>
2021-03-13 01:50:31 +0000

    Deploying to production from  @ d20c9d34ee8268561232361c8cd2b12584d2235b 🚀

---
+++ precache-manifest.2e09ecce7d946da2cb0ecd8dba718857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526b1beb4b5fd027101a87216613ad40",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4c1d6d487f258f33052",
+    "url": "./static/css/main.ef802da4.chunk.css"
+  },
+  {
+    "revision": "942395b23d0e57dbd9b7",
+    "url": "./static/js/2.c23acef9.chunk.js"
+  },
+  {
+    "revision": "8235eaab3319d8f76dec17a2b37a4f70",
+    "url": "./static/js/2.c23acef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c1d6d487f258f33052",
+    "url": "./static/js/main.8f43e70d.chunk.js"
+  },
+  {
+    "revision": "bbc2e182546b902270dd",
+    "url": "./static/js/runtime-main.a9f87361.js"
+  }
+]);
